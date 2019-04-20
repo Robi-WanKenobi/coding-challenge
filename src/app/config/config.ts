b064d8e95a0e170dfd327637895b226dc77c207f
rarranzt <roberto.arranz.93@gmail.com>
2019-04-20 14:04:48 +0200

    reformat search fetch contributors from api rest

---
+++ src/app/config/config.ts
@@ -0,0 +1,5 @@
+'use strict';
+
+export const gitHubToken = '03e6be2458e4c840d623a76bbfd58467a3ccb039';
+export const gitHubGraphqlRootUrl = 'https://api.github.com/graphql';
+export const gitHubApiRestRootUrl = 'https://api.github.com/';
