3edc7a22c8d529b37752a8c38a5e50b127c449f7
rarranzt <roberto.arranz.93@gmail.com>
2019-04-20 14:08:18 +0200

    remove token

--- src/app/config/config.ts
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-export const gitHubToken = '03e6be2458e4c840d623a76bbfd58467a3ccb039';
-export const gitHubGraphqlRootUrl = 'https://api.github.com/graphql';
-export const gitHubApiRestRootUrl = 'https://api.github.com/';
